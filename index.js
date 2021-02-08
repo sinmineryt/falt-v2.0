0ee3d654c93b2824730fa9764ff931a1904d3e66
Vee <58627044+Vee-Git@users.noreply.github.com>
2021-02-08 20:50:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
+client.on('message', message => {
+  if (message.content === '+ping') {  
+    message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  }
+});
+
 client.login(config.BOT_TOKEN);
